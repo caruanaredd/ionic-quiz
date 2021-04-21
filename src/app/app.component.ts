2b8e6ecbf5372d74d261f2f3436ee0b68e18227f
caruanaredd <redd.caruana@mcast.edu.mt>
2021-04-21 10:47:25 +0200

    Final Menu
    Incorporated the menu items.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,25 @@ import { Component } from '@angular/core';
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent
+{
+  pages: any = [
+    {
+      url: '/question',
+      name: 'Quiz',
+      icon: 'school'
+    },
+    {
+      url: '/profile',
+      name: 'Profile',
+      icon: 'person'
+    },
+    {
+      url: '/history',
+      name: 'History',
+      icon: 'checkmark-circle'
+    }
+  ];
+
   constructor() {}
 }
