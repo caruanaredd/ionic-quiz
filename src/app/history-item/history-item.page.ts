d1a64b6586dbcc09e0d1878fc2c98d1bee7ad595
caruanaredd <redd.caruana@mcast.edu.mt>
2021-04-29 11:19:30 +0200

    Modal Control
    + Controlled the history item page as a modal window.
    + Added clear button.

--- src/app/history-item/history-item.page.ts
+++ src/app/history-item/history-item.page.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AlertController, ModalController } from '@ionic/angular';
+import { NotificationsService } from '../services/notifications.service';
 import { StorageService } from '../services/storage.service';
 import { HistoryItem } from '../struct/question';
 
@@ -10,30 +12,101 @@ import { HistoryItem } from '../struct/question';
 })
 export class HistoryItemPage implements OnInit
 {
+  // The id of this record.
+  @Input('id') id: number = null;
+
   // The history item being viewed.
   data: HistoryItem = null;
 
   // The duration of this session.
   duration: number = 0;
 
+  // True if this is a modal window.
+  isModal: boolean = false;
+
   constructor(
+    private alertCtrl: AlertController,
+    private modalCtrl: ModalController,
     private route: ActivatedRoute,
+    private router: Router,
+    
+    private notifService: NotificationsService,
     private storageService: StorageService
   ) { }
 
   async ngOnInit()
   {
+    // Check if this is a modal window.
+    this.modalCtrl.getTop().then(value => this.isModal = value !== undefined);
+
     // Get the ID.
-    const id = this.route.snapshot.params['id'];
+    if (this.id == null)
+    {
+      this.id = this.route.snapshot.params['id'];
+    }
     
     // Get all the history.
     const history: HistoryItem[] = await this.storageService.get('history') || [];
 
-    if (history.length > id)
+    if (history.length > this.id)
     {
-      this.data = history[id];
+      this.data = history[this.id];
       this.duration = this.data.endTime - this.data.startTime;
     }
   }
 
+  /**
+   * Deletes the item and dismisses the modal or returns to the history page.
+   */
+  async delete()
+  {
+    const alert = await this.alertCtrl.create({
+      header: "Are you sure?",
+      message: "This action is permanent and cannot be undone.",
+      buttons: [
+        {
+          text: "No",
+          role: 'cancel'
+        },
+        {
+          text: "Yes",
+          handler: async () => {
+            // get the data from storage.
+            const history: HistoryItem[] = await this.storageService.get('history') || [];
+
+            // delete the array item.
+            if (history.length > this.id)
+            {
+              history.splice(this.id, 1);
+            }
+
+            // write the data again.
+            await this.storageService.set('history', history);
+
+            // redirect.
+            this.notifService.internalNotif("Item deleted.");
+
+            // Redirect if this is a page, dismiss if modal.
+            if (this.isModal)
+            {
+              this.modalCtrl.dismiss({ refresh: true });
+            }
+            else
+            {
+              this.router.navigateByUrl("/history", { replaceUrl: true });
+            }
+          }
+        }
+      ]
+    });
+    alert.present();
+  }
+
+  /**
+   * Closes the modal window.
+   */
+  closeModal()
+  {
+    this.modalCtrl.dismiss();
+  }
 }
--- src/app/history/history.page.ts
+++ src/app/history/history.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController, IonItemSliding, ModalController } from '@ionic/angular';
+import { HistoryItemPage } from '../history-item/history-item.page';
 import { NotificationsService } from '../services/notifications.service';
 import { StorageService } from '../services/storage.service';
 import { HistoryItem } from '../struct/question';
@@ -8,19 +10,51 @@ import { HistoryItem } from '../struct/question';
   templateUrl: './history.page.html',
   styleUrls: ['./history.page.scss'],
 })
-export class HistoryPage implements OnInit
+export class HistoryPage
 {
   // The quiz history.
   history: HistoryItem[] = [];
 
   constructor(
+    private alertCtrl: AlertController,
+    private modalCtrl: ModalController,
+
     private notifService: NotificationsService,
     private storageService: StorageService
   ) { }
 
-  async ngOnInit()
+  async ionViewWillEnter()
   {
     this.history = await this.storageService.get('history') || [];
+  }
+
+  /**
+   * Clears the history from storage.
+   */
+  async clear()
+  {
+    const alert = await this.alertCtrl.create({
+      header: "Are you sure?",
+      message: "Deleting your history is a permanent action and cannot be undone.",
+      buttons: [
+        {
+          text: "No",
+          role: 'cancel'
+        },
+        {
+          text: "Yes",
+          handler: async () => {
+            // Remove the history item from storage.
+            await this.storageService.remove('history');
+
+            // Reload the history array to refresh the page.
+            this.history = await this.storageService.get('history') || [];
+          }
+        }
+      ]
+    });
+
+    alert.present();
   }
 
   /**
@@ -37,5 +71,40 @@ export class HistoryPage implements OnInit
 
     // Show a confirmation message.
     this.notifService.internalNotif("Item deleted.");
+  }
+
+  /**
+   * Handles the slide event.
+   * @param event The item event, use $event in HTML.
+   * @param index The record index.
+   * @param item The Ionic Sliding Item, use ViewChild #option
+   */
+  async onSlide(event: any, index: number, item: IonItemSliding)
+  {
+    // Event ratio 1 is sliding towards the left (exposing the eye icon).
+    if (event.detail.ratio >= 1)
+    {
+      // Returns the top modal interface.
+      this.modalCtrl.getTop().then(async value => {
+        if (value !== undefined) return;
+
+        const modal = await this.modalCtrl.create({
+          component: HistoryItemPage,
+          componentProps: {
+            'id': index
+          }
+        });
+
+        modal.onWillDismiss().then(async response => {
+          if (response.data && response.data.refresh)
+          {
+            this.history = await this.storageService.get('history') || [];
+          }
+        });
+
+        setTimeout(() => item.close(), 100);
+        modal.present();
+      });
+    }
   }
 }
