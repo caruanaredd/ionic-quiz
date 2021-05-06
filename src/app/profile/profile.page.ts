e6692d1d270ded8036499b61e9421fbba91ee4de
caruanaredd <redd.caruana@mcast.edu.mt>
2021-05-06 13:51:33 +0200

    Camera Photo handling
    + Added code to handle camera photos in the interface.
    + Applied Modal Controller code to profile page.

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ModalController } from '@ionic/angular';
+import { GalleryPage } from '../gallery/gallery.page';
 import { QuizService } from '../services/quiz.service';
 import { StorageService } from '../services/storage.service';
 
@@ -15,7 +17,12 @@ export class ProfilePage implements OnInit
   // The user's surname.
   public surname: string = "Caruana";
 
+  // The user's profile picture.
+  public avatarSrc: string = null;
+
   constructor(
+    private modalCtrl: ModalController,
+
     public quizService: QuizService,
     private storageService: StorageService
   ) { }
@@ -24,6 +31,7 @@ export class ProfilePage implements OnInit
   {
     this.name = await this.storageService.get('name'); // || "Redd";
     this.surname = await this.storageService.get('surname'); // || "Caruana";
+    this.avatarSrc = await this.storageService.get('profilePic');
   }
 
   /**
@@ -34,5 +42,31 @@ export class ProfilePage implements OnInit
   onChange(key: string, e: any): void
   {
     this.storageService.set(key, e.detail.value);
+  }
+
+  async openGallery()
+  {
+    const modal = await this.modalCtrl.create({
+      component: GalleryPage
+    });
+
+    modal.onWillDismiss().then(response => {
+      if (response.role == 'cancel') return;
+
+      const photo = response.data;
+
+      // For mobile - we can use the filePath
+      // this.avatarSrc = photo.filePath;
+
+      // CHROME ONLY - we have to check our photo.
+      if (photo.base64Data) this.avatarSrc = photo.base64Data;
+      else if (photo.webViewPath) this.avatarSrc = photo.webViewPath;
+      else if (photo.filePath) this.avatarSrc = photo.filePath;
+      else this.avatarSrc = null;
+
+      this.storageService.set('profilePic', this.avatarSrc);
+    });
+
+    modal.present();
   }
 }
